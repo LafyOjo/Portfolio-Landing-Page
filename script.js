18e45d37e380c4ac66541ccfc6e92b4dffef540f
Isaiah Ojo <51814402+LafyOjo@users.noreply.github.com>
2022-04-09 04:11:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('scroll-up-btn').removeClass("show");
+        }
+    });
+    
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+    //toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+        
+    });
+    
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Designer", "Developer", "Blogger", "Investor"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    var typed = new Typed(".typing-2", {
+        strings: ["Designer", "Developer", "Blogger", "Investor"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    // owl carousel script js
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+            items: 1,
+            nav: false
+        },
+            600: {
+            items: 2,
+            nav: false
+        },
+            1000: {
+            items: 3,
+            nav: false
+        }
+      }  
+    });
+});
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+    cookieContainer.classList.remove("active");
+    localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout( () => {
+    if(!localStorage.getItem("cookieBannerDisplayed")){
+    cookieContainer.classList.add("active");
+    }
+}, 2000);
